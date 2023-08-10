df0af1c9db49645418aec6876089cebdb493b1c2
Lim Ethan <69660935+ethan-l-m-e@users.noreply.github.com>
2023-08-10 23:17:26 +0800

    Create Player class

---
+++ dodgeball/entities/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  dodgeball
+//
+//  Created by リム・イーサン on 10/8/23.
+//  Copyright © 2023 リム・イーサン. All rights reserved.
+//
+
+#include "Player.hpp"
---
+++ dodgeball/entities/Player.hpp
@@ -0,0 +1,18 @@
+//
+//  Player.hpp
+//  dodgeball
+//
+//  Created by リム・イーサン on 10/8/23.
+//  Copyright © 2023 リム・イーサン. All rights reserved.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+#include <stdio.h>
+
+class Player {
+    
+};
+
+#endif /* Player_hpp */
